955866c86cf026f3004711f5670712dc2d63e03a
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-06-20 22:33:37 +0300

    Filter blogs by author

--- src/Home.js
+++ src/Home.js
@@ -6,12 +6,13 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'Manasseh', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Kinyua', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Denis', id: 3 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Manasseh', id: 3 },
     ]);
 
   return (
     <div className="home">
         <BlogList blogs={blogs} title='My Blogs' />
+        <BlogList blogs={blogs.filter((blog) => blog.author === 'Manasseh')} title="Manasseh's Blogs" />
     </div>
   )
 }

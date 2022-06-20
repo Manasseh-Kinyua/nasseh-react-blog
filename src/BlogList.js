4ca7020f1245c1e5457901fa6f57cf8029258c54
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-06-20 23:16:47 +0300

    test useEffect hook

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const BlogList = ({blogs, title}) => {
+const BlogList = ({blogs, title, handleDelete}) => {
 
   return (
     <div className='blog-list'>
@@ -9,6 +9,7 @@ const BlogList = ({blogs, title}) => {
             <div className="blog-preview" key={blog.id}>
                 <h2>{ blog.title }</h2>
                 <p>Written By: { blog.author }</p>
+                <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
             </div>
         ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -9,10 +9,21 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Manasseh', id: 3 },
     ]);
 
+    // const [name, setName] = useState('Manasseh');
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id !==id);
+        setBlogs(newBlogs);
+    }
+
+    useEffect(() => {
+        console.log(blogs);
+    }, []);
+
   return (
     <div className="home">
-        <BlogList blogs={blogs} title='My Blogs' />
-        <BlogList blogs={blogs.filter((blog) => blog.author === 'Manasseh')} title="Manasseh's Blogs" />
+        <BlogList blogs={blogs} title='My Blogs' handleDelete={handleDelete} />
+        {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'Manasseh')} title="Manasseh's Blogs" /> */}
     </div>
   )
 }

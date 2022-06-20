0d7886af969b2a6fef0550d2279a67ea1cb171e6
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-06-20 22:22:13 +0300

    Add the bloglist component and use props to pass data to it

---
+++ src/BlogList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const BlogList = (props) => {
+    const blogs = props.blogs
+
+  return (
+    <div className='blog-list'>
+      {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+                <h2>{ blog.title }</h2>
+                <p>Written By: { blog.author }</p>
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default BlogList
--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,18 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'Manasseh', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Kinyua', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Denis', id: 3 }
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Denis', id: 3 },
     ]);
+
   return (
     <div className="home">
+        <h3>My Blogs</h3>
+        <BlogList blogs={blogs} />
     </div>
   )
 }

879c57437708f43675ca48817985f56b59ef5119
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-06-20 18:18:28 +0300

    Add the navbar and home component

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar">
+        <h1>Nasseh-Blog</h1>
+        <div className="links">
+            <a href="/">Home</a>
+            <a href="/create">New Blog</a>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar

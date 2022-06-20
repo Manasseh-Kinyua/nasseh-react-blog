0e7e52c3e00d6ea2f7dcd2fe742b4e43edc7a2ab
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-06-20 20:09:06 +0300

    Add state and som initial blogs

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'Manasseh', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Kinyua', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Denis', id: 3 }
+    ]);
   return (
     <div className="home">
-      <h2>Home Page</h2>
     </div>
   )
 }

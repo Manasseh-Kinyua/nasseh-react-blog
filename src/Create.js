67bd8900e197bac0e424b8833c95038cb59a5c54
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-06-21 11:17:50 +0300

    use controlled inputs to track user input

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,11 @@
+import { useState } from "react"
+
 const Create = () => {
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Manasseh');
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
@@ -7,19 +14,31 @@ const Create = () => {
         <input 
             type='text'
             required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
         />
 
         <label>Blog body:</label>
         <textarea
             required
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
         ></textarea>
-        
+
         <label>Blog author:</label>
-        <select>
+        <select
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+        >
             <option value='Manasseh'>Manasseh</option>
             <option value='Kinyua'>Kinyua</option>
         </select>
+
+        <button>Add Blog</button>
       </form>
+      <p>{title}</p>
+      <p>{body}</p>
+      <p>{author}</p>
     </div>
   )
 }

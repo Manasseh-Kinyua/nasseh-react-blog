bacd2416c84b5eeae0ceba165f728cd4691fb280
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-06-21 10:57:12 +0300

    add a form for adding a new blog

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,24 @@ const Create = () => {
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input 
+            type='text'
+            required
+        />
+
+        <label>Blog body:</label>
+        <textarea
+            required
+        ></textarea>
+        
+        <label>Blog author:</label>
+        <select>
+            <option value='Manasseh'>Manasseh</option>
+            <option value='Kinyua'>Kinyua</option>
+        </select>
+      </form>
     </div>
   )
 }

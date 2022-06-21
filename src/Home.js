9fd56b5cc6af94202e79d0ca20bfd51b26f7880c
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-06-21 09:27:36 +0300

    Handle errors

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     // const [name, setName] = useState('Manasseh');
 
@@ -12,17 +13,26 @@ const Home = () => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
             .then(res => {
+                if(!res.ok) {
+                    throw Error("Could not fetch the data for that resource")
+                }
                 return res.json();
             })
             .then((data) => {
                 setBlogs(data)
                 setIsPending(false)
+                setError(null)
+            })
+            .catch(err => {
+                setIsPending(false)
+                setError(err.message)
             })
         }, 1000);
     }, []);
 
   return (
     <div className="home">
+        {error && <div>{error}</div>}
         {isPending && <div>Loading.........</div>}
         {blogs && <BlogList blogs={blogs} title='My Blogs' />}
         {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'Manasseh')} title="Manasseh's Blogs" /> */}

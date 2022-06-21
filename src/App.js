757828720c275acafed17a4df2d79f4a6bf0a1fe
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-06-21 10:21:38 +0300

    Add a blog details page and route it

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
           
           <Route path='/' element= {<Home />} exact/>
           <Route path='/create' element= {<Create />} />
+          <Route path='/blogs/:id' element= {<BlogDetails />} />
 
         </Routes>
       </div>
---
+++ src/BlogDetails.js
@@ -0,0 +1,9 @@
+const BlogDetails = () => {
+  return (
+    <div className="blog-details">
+      <h2>Blog Details</h2>
+    </div>
+  )
+}
+
+export default BlogDetails
